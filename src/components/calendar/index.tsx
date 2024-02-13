9a5e45543e2930534a513c48ea38a4ec5ffdf96d
Alejandro <admin@mealmentor.app>
2024-02-13 21:50:59 +0000

    Create calendar views component

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -1,5 +1,26 @@
-import { JSX, FC } from "react"
+import { FC, JSX } from "react"
+import { Table } from "antd"
 
-export const Calendar: FC = (): JSX.Element => {
-    return <>Calendar</>
+export const Day: FC = (): JSX.Element => {
+    return (
+        <Table 
+            title={() => <>Day</>}
+        />
+    )
+}
+
+export const Week: FC = (): JSX.Element => {
+    return (
+        <Table 
+            title={() => <>Week</>}
+        />
+    )
+}
+
+export const Month: FC = (): JSX.Element => {
+    return (
+        <Table 
+            title={() => <>Month</>}
+        />
+    )
 }

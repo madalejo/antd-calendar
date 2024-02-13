71dc4c9aaa29424458f72ca1f17bdbb389965d57
Alejandro <admin@mealmentor.app>
2024-02-13 17:36:01 +0000

    Add filtering elements

--- src/components/filter/index.tsx
+++ src/components/filter/index.tsx
@@ -1,8 +1,75 @@
-import { JSX, FC } from "react"
+import { JSX, FC, CSSProperties, useState } from "react"
+import { Button, Radio, Space } from "antd"
+import { LeftOutlined } from '@ant-design/icons' // Import Ant icons
+
+import { Icon } from "@iconify/react"
+
+const containerStyle: CSSProperties = {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between"
+}
+
+const radioOptions = [
+    {
+        label: 'Day',
+        value: 0,
+        key: 0,
+    },
+    {
+        label: 'Week',
+        value: 1,
+        key: 1,
+    },
+    {
+        label: 'Month',
+        value: 2,
+        key: 2,
+    }
+]
 
 const FilterBar: FC = (): JSX.Element => {
+    const [view, setView] = useState<number>(0)
+
+    const handleView = (value: number): void => {
+        setView(value)
+    }
+
     return (
-        <>Filter Bar</>
+        <Space
+            style={containerStyle}
+        >
+            <Space>
+                <Button>Today</Button>
+                <Button 
+                    icon={
+                        <Icon 
+                            icon="heroicons-outline:chevron-left" 
+                            inline
+                        />
+                    }
+                />
+                <Button 
+                    icon={
+                        <Icon 
+                            icon="heroicons-outline:chevron-right" 
+                            inline
+                        />
+                    }
+                />
+            </Space>
+            <Space>
+                <Radio.Group 
+                    options={radioOptions}
+                    value={view}
+                    defaultValue={view}
+                    optionType="button"
+                    onChange={
+                        (e) => handleView(Number(e.target.value))
+                    }
+                />
+            </Space>
+        </Space>
     )
 }
 
